32a2869a4c8c9c65fa15fde00b0a7b5e5b571bd5
amogh4319 <amogh4319@gmail.com>
2023-08-08 17:35:45 +0530

    adding loader button visible

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React,{useState} from 'react';
-
+import {Spinner} from 'react-bootstrap';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
 const [movies,setMovies]=useState([]);
+const [isLoading,setIsLoading]=useState(false);
   async function fetchMoviesHandler(){
+    setIsLoading(true);
    const response=await fetch('https://swapi.dev/api/films/')
    const data=await response.json()
    
@@ -17,6 +19,7 @@ const [movies,setMovies]=useState([]);
         openingText:moviesData.opening_crawl
      } })
       setMovies(transformedMovies);
+      setIsLoading(false);
     }
   
 
@@ -26,7 +29,12 @@ const [movies,setMovies]=useState([]);
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+      {!isLoading&& movies.length>0 && <MoviesList movies={movies} />}
+      {!isLoading&& movies.length===0 && <p>No movies</p>}
+      {isLoading&&  <Spinner animation="border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>}
+      
       </section>
     </React.Fragment>
   );
